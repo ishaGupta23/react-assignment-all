2045c616b85dc85999627df6e912c33312619347
ishaGupta23 <126656250+ishaGupta23@users.noreply.github.com>
2023-04-12 01:38:48 +0530

    Update App.js
    We can fetch api data by and remove data by id have a remove button on it.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from "react";
 
-function App() {
+function FetchApiList_rmove() {
+  const apiUrl = "https://jsonplaceholder.typicode.com/posts";
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    fetch(apiUrl)
+      .then((response) => response.json())
+      .then((posts) => setPosts(posts));
+    
+  }, []);
+
+  const handleRemoveClick = async (postId) => {
+    try {
+      const response = await fetch(`${apiUrl}/${postId}`, { method: "DELETE" });
+      if (!response.ok) {
+        throw new Error("Failed to delete post");
+      }
+      setPosts(posts.filter((post) => post.id !== postId));
+    } catch (error) {
+      console.error("Error deleting post:", error);
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <table>
+      <thead>
+        <tr>
+          <th>ID</th>
+          <th>Title</th>
+          <th>Body</th>
+          <th>Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        {posts.map((post) => (
+          <tr key={post.id}>
+            <td>{post.id}</td>
+            <td>{post.title}</td>
+            <td>{post.body}</td>
+            <td>
+              <button onClick={() => handleRemoveClick(post.id)}>Remove</button>
+            </td>
+          </tr>
+        ))}
+      </tbody>
+    </table>
   );
 }
 
-export default App;
+export default FetchApiList_rmove;
+
