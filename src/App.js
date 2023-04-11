24d4a519cc7566783eccad99e98720c5e53cd213
ishaGupta23 <126656250+ishaGupta23@users.noreply.github.com>
2023-04-12 01:33:46 +0530

    Update App.js
    we can add email name and it will shown in same page and also we can update delete and add.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,114 @@
-import logo from './logo.svg';
-import './App.css';
 
-function App() {
+import React, { useState } from "react";
+
+
+function UserForm() {
+  // State to keep track of the user input
+  const [formData, setFormData] = useState({
+    name: "",
+    age: "",
+    email: "",
+  });
+
+  // State to keep track of the list of users
+  const [users, setUsers] = useState([]);
+
+  //  handle changes to the input fields
+  function handleChange(event) {
+    const { name, value } = event.target;
+    setFormData((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  }
+
+  //  handle adding a new user
+  function handleAddUser(event) {
+    event.preventDefault();
+    setUsers((prevUsers) => [...prevUsers, formData]);
+    setFormData({
+      name: "",
+      age: "",
+      email: "",
+    });
+  }
+
+  //  handle deleting a user by index
+  function handleDeleteUser(index) {
+    setUsers((prevUsers) =>
+      prevUsers.filter((user, i) => i !== index)
+    );
+  }
+
+  //  handle updating a user by index
+  function handleUpdateUser(index, updatedData) {
+    setUsers((prevUsers) =>
+      prevUsers.map((user, i) => (i === index ? updatedData : user))
+    );
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div >
+      <form onSubmit={handleAddUser}>
+        <label>
+          Name:
+          <input
+            type="text"
+            name="name"
+            value={formData.name}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Age:
+          <input
+            type="number"
+            name="age"
+            value={formData.age}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <label>
+          Email:
+          <input
+            type="email"
+            name="email"
+            value={formData.email}
+            onChange={handleChange}
+          />
+        </label>
+        <br />
+        <button type="submit">Add User</button>
+      </form>
+      <hr />
+      <h2>Users:</h2>
+      <ul>
+        {users.map((user, i) => (
+          <li key={i}>
+            {user.name}, {user.age}, {user.email}
+            <br />
+            <button onClick={() => handleDeleteUser(i)}>Delete</button>
+            <button
+              onClick={() => {
+                const updatedData = {
+                  name: prompt("Enter updated name:", user.name),
+                  age: prompt("Enter updated age:", user.age),
+                  email: prompt("Enter updated email:", user.email),
+                };
+                handleUpdateUser(i, updatedData);
+              }}
+            >
+              Update
+            </button>
+          </li>
+        ))}
+      </ul>
     </div>
   );
 }
 
-export default App;
+export default UserForm;
+
+
